689ca4eb0cc2dae7e1f656e560ca7130626a69cf
acrosson <crossonaj@gmail.com>
2026-02-18 12:20:26 -0500

    Added Google tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -42,6 +42,22 @@ export default function RootLayout({
             `
           }}
         />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-GLD4MP5ZN8"
+          strategy="afterInteractive"
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-GLD4MP5ZN8');
+            `
+          }}
+        />
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
